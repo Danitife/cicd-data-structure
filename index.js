e978e18aa5ac0d8fb321196e11b64f5a530c8d7e
Danitife <boluwatifedaniell29@gmail.com>
2026-03-18 10:06:02 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+// Initialize your project npm init
+// Installed express (the node framework) npm install express
+// Importing express using require
+
+// Request methods
+
+const express = require("express");
+const app = express();
+const PORT = 2222;
+
+app.get("/", (request, response)=>{
+    response.send("Welcome to my Node Class");
+})
+
+app.get("/get-names", (req, res)=>{
+    res.json(["Daniel", "Famous", "Yinka", "Temmy", "Oladayo", "Blessing", "Shammah"])
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`);
+})
+
+
+// https://dummyjson.com/products
